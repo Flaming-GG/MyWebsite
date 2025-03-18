89176e128499ef509e70f582821e59ce01b9a026
Flaming-GG <AbhilashA6@hwbcymru.net>
2025-03-18 13:15:34 +0000

    polished it up

--- colorScript.js
+++ colorScript.js
@@ -1,6 +1,17 @@
 function ChangeColor() {
-    const colors = ["#FFFFFF", "#000000", "##31e8d6"];
+    const colors = ["#FFFFFF", "#000000", "#31e8d6"];
     const RandomColor = colors[Math.floor(Math.random() * colors.length)]
 
     document.body.style.backgroundColor = RandomColor
+    if (RandomColor === "#000000")
+    {
+        document.getElementById("text_01").style.color = "#FFFFFF";
+        document.getElementById("title").style.color = "#FFFFFF"
+    }
+    if (RandomColor === "#FFFFFF" || RandomColor === "#31e8d6")
+    {
+        document.getElementById("text_01").style.color = "#000000";
+    }
+
+    
 }
